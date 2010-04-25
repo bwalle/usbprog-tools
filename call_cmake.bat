725274f203f9119563b68f30ea4981803e98a6dd
Bernhard Walle <bernhard@bwalle.de>
2010-04-25 14:33:41 +0100

    Add call_cmake.bat
    Convenience batch file for MS Windows.
    
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

---
+++ call_cmake.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set LIBUSB_INCLUDE_DIR=C:\Users\bwalle\devel\libusb-pbatard\libusb
+set LIBUSB_LIBDIR=C:\Users\bwalle\devel\libusb-pbatard\Win32\Release\dll
+set GENERATOR="NMake Makefiles"
+rem set GENERATOR="Visual Studio 9 2008"
+
+cd build
+cmake -G %GENERATOR% -DLIBUSB_ADDITIONAL_INCLUDEDIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_ADDITIONAL_LIBDIR=%LIBUSB_LIBDIR% ..
+cd ..
