3beaa1fb137d1aa3a5072b1b377bb605be1e7542
Bernhard Walle <bernhard@bwalle.de>
2011-03-05 19:45:42 +0100

    Update libbw to 0.2.4
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

---
+++ libbw/os.h
@@ -0,0 +1,57 @@
+/* {{{
+ * Copyright (c) 2007-2010, Bernhard Walle <bernhard@bwalle.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <copyright holder> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. }}}
+ */
+#ifndef OS_H
+#define OS_H
+
+/**
+ * @file
+ * @brief Operating system abstraction functions
+ *
+ * This file contains operating system specific functions.
+ *
+ * @author Bernhard Walle <bernhard@bwalle.de>
+ */
+
+namespace bw {
+
+/**
+ * @brief Puts the current process to background.
+ *
+ * Currently the function is implemented in POSIX only.
+ *
+ * The function is for programs wishing to detach themselves from the
+ * controlling terminal and run in the background as system daemons.
+ *
+ * @return 0 on success, any standard error code on failure.
+ */
+int daemonize();
+
+} // end namespace bw
+
+#endif /* OS_H */
+
+// vim: set sw=4 ts=4 et fdm=marker:
