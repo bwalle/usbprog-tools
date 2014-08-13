a9642d41716bae4aab9f35abcd859b473a639cc9
Bernhard Walle <bernhard@bwalle.de>
2014-08-13 15:34:22 +0200

    scripts/test_builds.sh: Test also USE_LEGACY_LIBUSB
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- scripts/test_builds.sh
+++ scripts/test_builds.sh
@@ -20,9 +20,9 @@ test_variant()
     echo ""
     echo ""
     highlight_on
-    echo "---------------------------------------------------------"
-    echo "   *** compiler=$1 $2 $3 $4 ***"
-    echo "---------------------------------------------------------"
+    echo "-----------------------------------------------------------------------------------"
+    echo "   *** compiler=$1 $2 $3 $4 $5 ***"
+    echo "-----------------------------------------------------------------------------------"
     default
     echo ""
 
@@ -46,7 +46,7 @@ test_variant()
         -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
         "-D$2" "-D$3" "-D$4" ..
     if ! make ; then
-        echo "   Variant FAILED: *** compiler=$1 $2 $3 $4 ***"
+        echo "   Variant FAILED: *** compiler=$1 $2 $3 $4 $5 ***"
     fi
     cd ..
     rm -fr build
@@ -54,9 +54,17 @@ test_variant()
 }
 
 for compiler in gcc clang ; do
-    test_variant $compiler BUILD_ONLY_CORE=ON USE_QT5=OFF BUILD_GUI=OFF
-    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=OFF BUILD_GUI=OFF
-    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=OFF BUILD_GUI=ON
-    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=ON BUILD_GUI=OFF
-    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=ON BUILD_GUI=ON
+    test_variant $compiler BUILD_ONLY_CORE=ON USE_QT5=OFF BUILD_GUI=OFF USE_LEGACY_LIBUSB=OFF
+    test_variant $compiler BUILD_ONLY_CORE=ON USE_QT5=OFF BUILD_GUI=OFF USE_LEGACY_LIBUSB=ON
+
+    for qt5 in OFF ON ; do
+        for build_gui in OFF ON ; do
+            for legacy_libusb in OFF ON ; do
+                test_variant $compiler BUILD_ONLY_CORE=OFF \
+                    USE_QT5=$qt5 \
+                    BUILD_GUI=$build_gui \
+                    USE_LEGACY_LIBUSB=$legacy_libusb
+            done
+        done
+    done
 done
