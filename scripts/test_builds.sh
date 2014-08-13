fc4371a8af26cff31f5e9c96b4c7aa2b3f3ddbd7
Bernhard Walle <bernhard@bwalle.de>
2014-08-13 15:00:56 +0200

    scripts/test_builds.sh: Add clang test
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- scripts/test_builds.sh
+++ scripts/test_builds.sh
@@ -21,23 +21,42 @@ test_variant()
     echo ""
     highlight_on
     echo "---------------------------------------------------------"
-    echo "   *** $1 $2 $3 ***"
+    echo "   *** compiler=$1 $2 $3 $4 ***"
     echo "---------------------------------------------------------"
     default
     echo ""
+
+    if [ "$1" = "gcc" ] ; then
+        C_COMPILER=gcc
+        CXX_COMPILER=g++
+    elif [ "$1" = "clang" ] ; then
+        C_COMPILER=clang
+        CXX_COMPILER=clang++
+    else
+        echo "Compiler '$1' not supported"
+        exit 1
+    fi
 
     cd "$BASEDIR"
     rm -fr build
     mkdir build
     cd build
-    cmake "-D$1" "-D$2" "-D$3" ..
-    make
+    cmake \
+        -DCMAKE_C_COMPILER="$C_COMPILER" \
+        -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
+        "-D$2" "-D$3" "-D$4" ..
+    if ! make ; then
+        echo "   Variant FAILED: *** compiler=$1 $2 $3 $4 ***"
+    fi
     cd ..
     rm -fr build
+    return $ret
 }
 
-test_variant BUILD_ONLY_CORE=ON USE_QT5=OFF BUILD_GUI=OFF
-test_variant BUILD_ONLY_CORE=OFF USE_QT5=OFF BUILD_GUI=OFF
-test_variant BUILD_ONLY_CORE=OFF USE_QT5=OFF BUILD_GUI=ON
-test_variant BUILD_ONLY_CORE=OFF USE_QT5=ON BUILD_GUI=OFF
-test_variant BUILD_ONLY_CORE=OFF USE_QT5=ON BUILD_GUI=ON
+for compiler in gcc clang ; do
+    test_variant $compiler BUILD_ONLY_CORE=ON USE_QT5=OFF BUILD_GUI=OFF
+    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=OFF BUILD_GUI=OFF
+    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=OFF BUILD_GUI=ON
+    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=ON BUILD_GUI=OFF
+    test_variant $compiler BUILD_ONLY_CORE=OFF USE_QT5=ON BUILD_GUI=ON
+done
