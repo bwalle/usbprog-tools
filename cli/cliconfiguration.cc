5463d0fb856acd4edd6296e104b882334fb9ac2a
Bernhard Walle <bernhard@bwalle.de>
2014-08-16 19:08:58 +0200

    Use system proxy settings
    We have to add it to GuiConfiguration and CliConfiguration since the base
    class is independent of Qt.
    
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- cli/cliconfiguration.cc
+++ cli/cliconfiguration.cc
@@ -1,5 +1,5 @@
 /*
- * (c) 2007-2010, Bernhard Walle <bernhard@bwalle.de>
+ * (c) 2007-2014, Bernhard Walle <bernhard@bwalle.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <QNetworkProxyFactory>
+
 #include "cliconfiguration.h"
 
 namespace usbprog {
@@ -25,7 +28,9 @@ CliConfiguration *CliConfiguration::m_instance = NULL;
 
 /* -------------------------------------------------------------------------- */
 CliConfiguration::CliConfiguration()
-{}
+{
+    QNetworkProxyFactory::setUseSystemConfiguration(true);
+}
 
 /* -------------------------------------------------------------------------- */
 CliConfiguration::~CliConfiguration()
--- gui/guiconfiguration.cc
+++ gui/guiconfiguration.cc
@@ -1,5 +1,5 @@
 /*
- * (c) 2007-2010, Bernhard Walle <bernhard@bwalle.de>
+ * (c) 2007-2014, Bernhard Walle <bernhard@bwalle.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <QNetworkProxyFactory>
+
 #include "guiconfiguration.h"
 
 namespace usbprog {
@@ -23,7 +26,9 @@ namespace gui {
 
 /* -------------------------------------------------------------------------- */
 GuiConfiguration::GuiConfiguration()
-{}
+{
+    QNetworkProxyFactory::setUseSystemConfiguration(true);
+}
 
 /* -------------------------------------------------------------------------- */
 GuiConfiguration::~GuiConfiguration()
